b22c35c5fad8e2f39013c59ea0f0ef28cc7c007b
shrirs@microsoft.com <shrirs@microsoft.com>
2017-05-08 23:32:04 -0700

    Minor name changes to SQL Queue scripts

--- src/WebJobs.Extensions.SqlQueue/scripts/dequeue.sql
+++ src/WebJobs.Extensions.SqlQueue/scripts/dequeue.sql
@@ -1,14 +1,18 @@
 -- Copyright (c) .NET Foundation. All rights reserved.
 -- Licensed under the MIT License. See License.txt in the project root for license information.
 
-USE [webjobs]
+USE [SqlQueue]
 GO
 
 BEGIN TRANSACTION
 
-WAITFOR (RECEIVE TOP(1) CONVERT(VarChar(MAX), message_body) AS C FROM [RECEIVERQUEUE1]), TIMEOUT 2000
+WAITFOR (RECEIVE TOP(1) CONVERT(VarChar(MAX), message_body) AS MyContract FROM [MyQueue]), TIMEOUT 2000
 
 COMMIT TRANSACTION
 
 -- Read all messages. This will block if a transaction is in progress
-SELECT CONVERT(VarChar(MAX), message_body), status from RECEIVERQUEUE1
+-- SELECT CONVERT(VarChar(MAX), message_body), status from MyQueue
+
+-- WAITFOR (RECEIVE TOP(1) CONVERT(VarChar(Max), message_body) AS MyContract FROM MyQueue), TIMEOUT 10000
+
+
--- src/WebJobs.Extensions.SqlQueue/scripts/enqueue.sql
+++ src/WebJobs.Extensions.SqlQueue/scripts/enqueue.sql
@@ -1,7 +1,7 @@
 -- Copyright (c) .NET Foundation. All rights reserved.
 -- Licensed under the MIT License. See License.txt in the project root for license information.
 
-USE [webjobs]
+USE [SqlQueue]
 GO
 
 BEGIN TRANSACTION
@@ -9,14 +9,16 @@ BEGIN TRANSACTION
 DECLARE @conversationID UNIQUEIDENTIFIER
 
 BEGIN DIALOG @conversationID
-              FROM SERVICE SENDERSERVICE
-              TO SERVICE 'RECEIVERSERVICE1'
-			  ON CONTRACT C
+              FROM SERVICE [SenderService]
+              TO SERVICE 'ReceiverService'
+			  ON CONTRACT MyContract
               WITH ENCRYPTION = OFF;
 
-SEND ON CONVERSATION @conversationID MESSAGE TYPE [MT] ('Dummy Message1');
-
-SEND ON CONVERSATION @conversationID MESSAGE TYPE [MT] ('Dummy Message2');
+SEND ON CONVERSATION @conversationID MESSAGE TYPE [MyMessageType] ('Message #1');
+SEND ON CONVERSATION @conversationID MESSAGE TYPE [MyMessageType] ('Message #2');
+SEND ON CONVERSATION @conversationID MESSAGE TYPE [MyMessageType] ('Message #3');
+SEND ON CONVERSATION @conversationID MESSAGE TYPE [MyMessageType] ('Message #4');
+SEND ON CONVERSATION @conversationID MESSAGE TYPE [MyMessageType] ('Message #5');
 
 -- This is optional. This adds a NULL message
 END CONVERSATION @conversationID
